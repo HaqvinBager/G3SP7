d59821b398f79c109c18fa2773566b0bae598b2b
AxelSavage <35657878+AxelSavage@users.noreply.github.com>
2021-02-15 17:41:35 +0100

    addition to folder utility

--- IronWrought/Source/Engine/FolderUtility.h
+++ IronWrought/Source/Engine/FolderUtility.h
@@ -19,14 +19,24 @@ public:
 	}
 
 
-	static std::vector<std::string> GetFilePathsInFolder(const std::string& aFolder, const std::string& aExtansion) {
+	static std::vector<std::string> GetFilePathsInFolder(const std::string& aFolder, const std::string& aExtansion, const std::string& aContains = "") {
 
 		std::vector<std::string> filePaths;
 		for (const auto& file : std::filesystem::directory_iterator(aFolder)) {
 			if (file.path().extension().string() == ".meta")
 				continue;
+
 			if (file.path().extension().string() == aExtansion) {
-				filePaths.emplace_back(file.path().filename().string());
+				if (aContains.size() > 0) {
+					if (file.path().string().find(aContains) != std::string::npos) {
+						filePaths.emplace_back(file.path().filename().string());
+					}
+				}
+				else {
+					if (file.path().string().find(aContains) != std::string::npos) {
+						filePaths.emplace_back(file.path().filename().string());
+					}
+				}
 			}
 		}
 		return filePaths;
