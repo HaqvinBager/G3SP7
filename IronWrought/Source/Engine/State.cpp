5ba7599e783195dbda4210c326ff5063e7619912
Aki <71226118+hyve-jpg@users.noreply.github.com>
2021-02-25 16:31:07 +0100

    Removal of source
    hey man its all

--- IronWrought/Source/Engine/State.cpp
+++
@@ -1,9 +0,0 @@
-#include "stdafx.h"
-#include "State.h"
-
-CState::CState(CStateStack& aStateStack, const CStateStack::EState aState): myStateStack(aStateStack), myState(aState) {
-
-}
-
-CState::~CState() {
-}
