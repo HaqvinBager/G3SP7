18f6eb71a809b772cdb87566df90684f0a43ac6c
AxelSavage <35657878+AxelSavage@users.noreply.github.com>
2021-02-25 15:46:49 +0100

    no message

--- IronWrought/Source/Engine/SceneManager.h
+++ IronWrought/Source/Engine/SceneManager.h
@@ -1,6 +1,5 @@
 #pragma once
 #include "Scene.h"
-#include "JsonReader.h"
 
 class CSceneManager
 {
@@ -8,23 +7,22 @@ public:
 	CSceneManager();
 	~CSceneManager();
 	static CScene* CreateEmpty();
-	static CScene* CreateScene(std::string ajsonFile);//Temp
-	static CScene* CreateScene(const std::vector<std::string>& someJsonFiles);//Temp
+	static CScene* CreateScene(const std::string& aSceneName);//Temp
+
+
+	//static CScene* CreateScene(std::string ajsonFile);//Temp
 
 private:
-	static bool IsValid(const rapidjson::Document& aDoc);
+	static bool AddGameObjects(CScene& aScene, const std::string& aJsonFileName);
+	static void SetTransforms(CScene& aScene, const std::string& aJsonFileName);
+	static void AddModelComponents(CScene& aScene, const std::string& aJsonFileName);
+	static void AddInstancedModelComponents(CScene& aScene, const std::string& aJsonFileName);
 
 
-	static bool AddGameObjects(CScene& aScene, const std::vector<std::string>& someJsonFiles);
-	static void SetTransforms(CScene& aScene, const std::vector<std::string>& someJsonFiles);
-	static void AddModelComponents(CScene& aScene, const std::vector<std::string>& someJsonFiles);
-	static void AddInstancedModelComponents(CScene& aScene, const std::vector<std::string>& someJsonFiles);
+	//static const std::string& Find(const std::vector<std::string>& someStrings, const std::string& aContains);
+	//static std::string npos;
 
 
-	static const std::string& Find(const std::vector<std::string>& someStrings, const std::string& aContains);
-
-
-	static std::string npos;
 	//static CScene* CreateScene(std::vector<std::string> ajsonFile);
 };
 
