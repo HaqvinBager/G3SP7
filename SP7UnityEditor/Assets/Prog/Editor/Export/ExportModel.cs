a1500c044617d48d85721104810fe4c0166404df
AxelSavage <35657878+AxelSavage@users.noreply.github.com>
2021-03-02 14:39:44 +0100

    Updates to Unity Export

--- SP7UnityEditor/Assets/Prog/Editor/Export/ExportModel.cs
+++ SP7UnityEditor/Assets/Prog/Editor/Export/ExportModel.cs
@@ -20,7 +20,7 @@ class ModelCollection
 
 public class ExportModel
 {
-    public static void Export(Scene aScene)
+    public static void Export(Scene aScene, List<int> validInstanceIds)
     {
         ModelCollection fbxLinks = new ModelCollection();
         fbxLinks.modelLinks = new List<ModelLink>();
@@ -37,22 +37,73 @@ public class ExportModel
                 if (renderer.TryGetComponent(out MeshFilter meshFilter))
                 {
                     GameObject modelAsset = PrefabUtility.GetCorrespondingObjectFromOriginalSource(meshFilter).gameObject;
-                    if(modelAsset.transform.parent != null)
-                    {
-                        modelAsset = modelAsset.transform.parent.gameObject;
-                    }
                     
-
                     if (modelAsset == null)
                         continue;
 
-                    ModelLink link = new ModelLink();
-                    link.assetID = modelAsset.transform.GetInstanceID();
-                    link.instanceID = prefabParent.transform.GetInstanceID();
-                    fbxLinks.modelLinks.Add(link);
+                    if (modelAsset.transform.parent != null)
+                        modelAsset = modelAsset.transform.parent.gameObject;
+
+                    if (prefabParent.name.Contains("BP_"))
+                    {
+
+                        Renderer[] childRenderers = prefabParent.GetComponentsInChildren<Renderer>(); 
+                        foreach(Renderer childRenderer in childRenderers)
+                        {
+                            if(GetNearestPrefabInstance(childRenderer, out Transform validPrefabParent, validInstanceIds))
+                            {
+                                ModelLink link = new ModelLink();
+                                link.assetID = modelAsset.transform.GetInstanceID();
+                                link.instanceID = validPrefabParent.transform.GetInstanceID();
+
+                                if(!fbxLinks.modelLinks.Exists(e => e.instanceID == link.instanceID))
+                                {
+                                    fbxLinks.modelLinks.Add(link);
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        ModelLink link = new ModelLink();
+                        link.assetID = modelAsset.transform.GetInstanceID();
+                        link.instanceID = prefabParent.transform.GetInstanceID();
+                        fbxLinks.modelLinks.Add(link);
+                    }
                 }
             }           
         }
         Json.ExportToJson(fbxLinks, aScene.name);
     }
+
+    public static bool GetNearestPrefabInstance<T>(T aInstance, out Transform outNearestPrefabInstance, List<int> validInstanceIDs) where T : Component
+    {
+        outNearestPrefabInstance = null;
+
+        T source = PrefabUtility.GetCorrespondingObjectFromOriginalSource(aInstance);
+        if (source != null)
+        {
+            outNearestPrefabInstance = GetNearestValidParent(aInstance.transform, validInstanceIDs);
+        }
+        return outNearestPrefabInstance != null;
+    }
+
+    private static Transform GetNearestValidParent(Transform aChild, List<int> validInstanceIDs)
+    {
+        if (aChild.parent != null)
+        {
+            if (validInstanceIDs.Contains(aChild.parent.GetInstanceID()))
+            {
+                return aChild.parent;
+            }
+            else
+            {
+                return GetNearestValidParent(aChild.parent, validInstanceIDs);
+            }
+        }
+
+        return null;
+    }
+
+
 }
--- SP7UnityEditor/Assets/Prog/Editor/Export/ExportTransform.cs
+++ SP7UnityEditor/Assets/Prog/Editor/Export/ExportTransform.cs
@@ -21,25 +21,49 @@ public struct TransformCollection
 
 public class ExportTransform
 {
-    public static void Export(Scene aScene)
+    public static void Export(Scene aScene, List<int> validInstanceIDs)
     {
         TransformCollection transformCollection = new TransformCollection();
         transformCollection.transforms = new List<TransformLink>();
         Transform[] transforms = GameObject.FindObjectsOfType<Transform>();
         foreach (Transform transform in transforms)
         {
-            if(Json.TryIsValidExport(transform, out GameObject prefabParent))
+            if (validInstanceIDs.Contains(transform.GetInstanceID()))
             {
-                if (transformCollection.transforms.Exists(e => e.instanceID == prefabParent.transform.GetInstanceID()))
-                    continue;
-
                 TransformLink link = new TransformLink();
-                link.instanceID = prefabParent.transform.GetInstanceID();               
+                link.instanceID = transform.GetInstanceID();
                 link.position = transform.ConvertToIronWroughtPosition();
                 link.rotation = transform.ConvertToIronWroughtRotation();
-                link.scale = prefabParent.transform.ConvertToIronWroughtScale();
+                link.scale = transform.ConvertToIronWroughtScale();
                 transformCollection.transforms.Add(link);
             }
+
+
+            //if (Json.TryIsValidExport(transform, out GameObject prefabParent))
+            //{
+            //    if (prefabParent.name.Contains("BP_"))
+            //    {
+            //        Transform[] children = prefabParent.GetComponentsInChildren<Transform>();
+
+            //        foreach (Transform child in children)
+            //        {
+            //            GameObject nearestPrefabParent = PrefabUtility.GetNearestPrefabInstanceRoot(child.gameObject);
+
+            //        }
+            //    }
+
+            //    if (transformCollection.transforms.Exists(e => e.instanceID == transform.GetInstanceID()))
+            //        continue;
+
+            //    TransformLink link = new TransformLink();
+            //    link.instanceID = prefabParent.transform.GetInstanceID();
+            //    link.position = transform.ConvertToIronWroughtPosition();
+            //    link.rotation = transform.ConvertToIronWroughtRotation();
+            //    link.scale = prefabParent.transform.ConvertToIronWroughtScale();
+            //    transformCollection.transforms.Add(link);
+
+            //    //Debug.Log("Export Transform: " + transform.name, transform.gameObject);
+            //}
         }
         Json.ExportToJson(transformCollection, aScene.name);
     }
