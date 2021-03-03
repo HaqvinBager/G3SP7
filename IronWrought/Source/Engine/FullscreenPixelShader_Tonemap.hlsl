acdcb82e3b808e9e4b81d91374215952f6448294
Nicolas Risberg <35574154+nicolas-risberg@users.noreply.github.com>
2021-03-03 20:05:51 +0100

    added working aces

--- IronWrought/Source/Engine/FullscreenPixelShader_Tonemap.hlsl
+++ IronWrought/Source/Engine/FullscreenPixelShader_Tonemap.hlsl
@@ -12,40 +12,52 @@ float3 Uncharted2Tonemap(float3 x)
     return ((x * (A * x + C * B) + D * E) / (x * (A * x + B) + D * F)) - E / F;
 }
 
-float3x3 aces_input_matrix =
+// sRGB => XYZ => D65_2_D60 => AP1 => RRT_SAT
+static const float3x3 ACESInputMat =
 {
-    float3(0.59719f, 0.35458f, 0.04823f),
-    float3(0.07600f, 0.90834f, 0.01566f),
-    float3(0.02840f, 0.13383f, 0.83777f)
+    { 0.59719, 0.35458, 0.04823 },
+    { 0.07600, 0.90834, 0.01566 },
+    { 0.02840, 0.13383, 0.83777 }
 };
 
-float3x3 aces_output_matrix =
+// ODT_SAT => XYZ => D60_2_D65 => sRGB
+static const float3x3 ACESOutputMat =
 {
-    float3( 1.60475f, -0.53108f, -0.07367f),
-    float3(-0.10208f,  1.10813f, -0.00605f),
-    float3(-0.00327f, -0.07276f,  1.07602f)
+    { 1.60475, -0.53108, -0.07367 },
+    { -0.10208, 1.10813, -0.00605 },
+    { -0.00327, -0.07276, 1.07602 }
 };
 
-//vec3 mul(const std::array< vec3, 3>& m, const vec3& v)
-//{
-//    float x = m[0][0] * v[0] + m[0][1] * v[1] + m[0][2] * v[2];
-//    float y = m[1][0] * v[1] + m[1][1] * v[1] + m[1][2] * v[2];
-//    float z = m[2][0] * v[1] + m[2][1] * v[1] + m[2][2] * v[2];
-//    return vec3(x, y, z);
-//}
-
-float3 rtt_and_odt_fit(float3 v)
+float3 RRTAndODTFit(float3 v)
 {
     float3 a = v * (v + 0.0245786f) - 0.000090537f;
     float3 b = v * (0.983729f * v + 0.4329510f) + 0.238081f;
     return a / b;
 }
 
-float3 aces_fitted(float3 v)
+float3 ACESFitted(float3 color)
 {
-    v = mul(aces_input_matrix, v);
-    v = rtt_and_odt_fit(v);
-    return mul(aces_output_matrix, v);
+    color = mul(ACESInputMat, color);
+
+    // Apply RRT and ODT
+    color = RRTAndODTFit(color);
+
+    color = mul(ACESOutputMat, color);
+
+    // Clamp to [0, 1]
+    color = saturate(color);
+
+    return color;
+}
+
+float3 ACESApprox(float3 x)
+{
+    float a = 2.51f;
+    float b = 0.03f;
+    float c = 2.43f;
+    float d = 0.59f;
+    float e = 0.14f;
+    return saturate((x * (a * x + b)) / (x * (c * x + d) + e));
 }
 
 PixelOutput main(VertexToPixel input)
@@ -68,7 +80,7 @@ PixelOutput main(VertexToPixel input)
 	// Uncharted 2
 	{
         float3 whitePoint = 10.0f;
-        float exposure = 1.0f;
+        float exposure = 3.0f;
         returnValue.myColor.rgb = Uncharted2Tonemap(resource * exposure) / Uncharted2Tonemap(whitePoint);
     }
 	// Uncharted 2
@@ -77,10 +89,17 @@ PixelOutput main(VertexToPixel input)
     //{
     //    float3 whitePoint = 10.0f;
     //    float exposure = 3.0f;
-    //    returnValue.myColor.rgb = aces_fitted(resource * exposure) / aces_fitted(whitePoint);
+    //    returnValue.myColor.rgb = ACESFitted(resource * exposure) / ACESFitted(whitePoint);
     //}
     // ACES
-	
+    
+    // ACES approx
+    //{
+    //    float3 whitePoint = 10.0f;
+    //    float exposure = 1.0f;
+    //    returnValue.myColor.rgb = ACESApprox(resource * exposure) / ACESApprox(whitePoint);
+    //}
+    // ACES approx
 	
     returnValue.myColor.a = 0.0f;
     return returnValue;
