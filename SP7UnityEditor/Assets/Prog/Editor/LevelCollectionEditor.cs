4e942779387668881516d98952a0877f4fa722b5
AxelSavage <35657878+AxelSavage@users.noreply.github.com>
2021-03-22 15:56:10 +0100

    Can now open multiple all scenes in a LevelCollection

--- SP7UnityEditor/Assets/Prog/Editor/LevelCollectionEditor.cs
+++ SP7UnityEditor/Assets/Prog/Editor/LevelCollectionEditor.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEditor.SceneManagement;
 
 [CustomEditor(typeof(LevelCollection))]
 public class LevelCollectionEditor : Editor
@@ -11,14 +12,37 @@ public class LevelCollectionEditor : Editor
     public override void OnInspectorGUI()
     {
         base.OnInspectorGUI();
-        
+
+        serializedObject.Update();
+
         obj = EditorGUILayout.ObjectField("Scene Asset", obj, typeof(SceneAsset), false);
+       
+        SceneAsset sceneAsset = obj as SceneAsset;   
         if (GUILayout.Button("Add Scene"))
         {
-            LevelCollection levelCollection = (LevelCollection)target;
-            SceneReference sceneReference = new SceneReference();
-            sceneReference.ScenePath = AssetDatabase.GetAssetPath(obj.GetInstanceID());
-            levelCollection.myScenes.Add(sceneReference);          
+            var myScenes = serializedObject.FindProperty("myScenes");
+            int size = myScenes.arraySize;
+            myScenes.InsertArrayElementAtIndex(size);
+            var newElement = myScenes.GetArrayElementAtIndex(size);
+            newElement.FindPropertyRelative("sceneAsset").objectReferenceValue = obj;
+            obj = null;
         }
+
+
+        if(GUILayout.Button("Open Scenes"))
+        {
+            LevelCollection levelCollection = (LevelCollection)target;
+            EditorSceneManager.OpenScene(levelCollection.myScenes[0].ScenePath, OpenSceneMode.Single);
+            for (int i = 1; i < levelCollection.myScenes.Count; ++i)
+                EditorSceneManager.OpenScene(levelCollection.myScenes[i].ScenePath, OpenSceneMode.Additive);
+        }
+
+        if (GUILayout.Button("Export"))
+        {
+            
+
+        }
+
+        serializedObject.ApplyModifiedProperties();
     }
 }
