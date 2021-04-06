f98cb24c385191c31bf0ba3a0cece5a0d837c8d4
Nicolas Risberg <35574154+nicolas-risberg@users.noreply.github.com>
2021-04-06 17:41:42 +0200

    added sprite sheet animation to crosshair

--- IronWrought/Source/Engine/SpriteInstance.cpp
+++ IronWrought/Source/Engine/SpriteInstance.cpp
@@ -10,6 +10,9 @@ CSpriteInstance::CSpriteInstance(CScene& aScene, bool aAddToScene)
 	, myRenderOrder(ERenderOrder::BackgroundLayer)
 	, myShouldRender(true)
 	, myShouldAnimate(false)
+	, myAnimationTimer(0.0f)
+	, myCurrentAnimationIndex(0)
+	, myCurrentAnimationFrame(0)
 {
 	if (aAddToScene)
 		aScene.AddInstance(this);
@@ -39,7 +42,7 @@ bool CSpriteInstance::Init(CSprite* aSprite, const Vector2& aScale)
 	return true;
 }
 
-bool CSpriteInstance::Init(CSprite* aSprite, const SSpriteSheetPositionData& /*someSpriteSheetPositionData*/, const Vector2& aScale)
+bool CSpriteInstance::Init(CSprite* aSprite, const std::vector<SSpriteSheetPositionData>& someSpriteSheetPositionData, const Vector2& aScale)
 {
 	if (!aSprite) {
 		return false;
@@ -49,8 +52,36 @@ bool CSpriteInstance::Init(CSprite* aSprite, const SSpriteSheetPositionData& /*s
 	this->SetSize(aScale);
 	myRenderOrder = ERenderOrder::ForegroundLayer;
 
+	Vector2 sheetDimensions = aSprite->GetSpriteData().myDimensions;
 
+	for (unsigned int i = 0; i < someSpriteSheetPositionData.size(); ++i)
+	{
+		myAnimationData.emplace_back();
+		auto& animationDataToLoad = someSpriteSheetPositionData.back();
+		myAnimationData.back().myAnimationName = animationDataToLoad.myAnimationName;
+		myAnimationData.back().myFramesOffset = static_cast<int>(myAnimationFrames.size());
+		myAnimationData.back().myNumberOfFrames = animationDataToLoad.myNumberOfFrames;
+		myAnimationData.back().myFramesPerSecond = animationDataToLoad.mySpeedInFramesPerSecond;
 
+		for (unsigned int j = 0; j < static_cast<unsigned int>(animationDataToLoad.myNumberOfFrames); ++j)
+		{
+			myAnimationFrames.emplace_back();
+			auto& currentUV = myAnimationFrames.back();
+			currentUV.x = (animationDataToLoad.mySpriteWidth * j) / sheetDimensions.x; // Left
+			currentUV.y = (animationDataToLoad.myVerticalStartingPosition) / sheetDimensions.y; // Up
+			currentUV.z = (animationDataToLoad.mySpriteWidth * (j + 1)) / sheetDimensions.x; // Right
+			currentUV.w = (animationDataToLoad.myVerticalStartingPosition + animationDataToLoad.mySpriteHeight) / sheetDimensions.y; // Down
+		}
+	}
+
+	myShouldAnimate = !myAnimationFrames.empty();
+
+	if (myShouldAnimate)
+	{
+		Vector2 frameSize = { someSpriteSheetPositionData.back().mySpriteWidth,  someSpriteSheetPositionData.back().mySpriteHeight };
+		Vector2 scaleProportions = (frameSize / sheetDimensions);
+		this->SetSize(aScale * scaleProportions);
+	}
 
 	return true;
 }
@@ -62,6 +93,10 @@ void CSpriteInstance::SetSize(DirectX::SimpleMath::Vector2 aSize)
 	mySize = mySpriteData.myDimensions;
 	mySize /= 1080.0f;
 	mySize *= aSize;
+	//mySize = mySpriteData.myDimensions;
+	//mySize.x /= 1920.0f;
+	//mySize.y /= 1080.0f;
+	//mySize *= aSize;
 }
 
 void CSpriteInstance::SetShouldRender(bool aBool)
@@ -71,6 +106,19 @@ void CSpriteInstance::SetShouldRender(bool aBool)
 
 void CSpriteInstance::Update()
 {
+	if (!myShouldAnimate)
+		return;
+
+	if ((myAnimationTimer += CTimer::Dt()) > (1.0f / myAnimationData[myCurrentAnimationIndex].myFramesPerSecond))
+	{
+		myAnimationTimer = 0.0f; // doing it properly doesn't seem to work, as CTimer is not marked at the start of this state
+		myCurrentAnimationFrame++;
+		if (myCurrentAnimationFrame > (myAnimationData[myCurrentAnimationIndex].myNumberOfFrames + myAnimationData[myCurrentAnimationIndex].myFramesOffset - 1))
+		{
+			myCurrentAnimationFrame = myAnimationData[myCurrentAnimationIndex].myFramesOffset;
+		}
+		this->SetUVRect(myAnimationFrames[myCurrentAnimationFrame]);
+	}
 }
 
 void CSpriteInstance::SetRenderOrder(ERenderOrder aRenderOrder)
