6f58fa4fba110d823618179ef7369aa4de02330a
AxelSavage <35657878+AxelSavage@users.noreply.github.com>
2021-04-07 18:28:11 +0200

    Enemy Prefab

---
+++ SP7UnityEditor/Assets/Prog/EnemySettings.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct Enemy
+{
+    [HideInInspector]
+    public int instanceID;
+
+    public float speed;
+    public float distance;
+    public float radius;
+}
+
+public class EnemySettings : MonoBehaviour
+{
+    public Enemy settings = new Enemy { speed = 10.0f, distance = 0.0f, radius = 0.0015f };  
+}
