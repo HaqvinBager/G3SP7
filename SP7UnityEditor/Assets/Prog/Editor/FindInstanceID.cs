f10aef8fd186e6222d9ccb47b9a55b728f7fcd2c
AxelSavage <35657878+AxelSavage@users.noreply.github.com>
2021-04-13 16:10:58 +0200

    Parent Export FindInstanceID Script

---
+++ SP7UnityEditor/Assets/Prog/Editor/FindInstanceID.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class FindInstanceID : EditorWindow
+{
+    [MenuItem("Help/Find Instance ID")]
+    public static void Find()
+    {
+        EditorWindow.CreateWindow<FindInstanceID>();
+    }
+
+
+    int findInstanceID = 0;
+    private void OnGUI()
+    {
+        findInstanceID = EditorGUILayout.IntField("Instance ID: ", findInstanceID);
+        if (GUILayout.Button("Find"))
+        {
+            Transform[] transforms = GameObject.FindObjectsOfType<Transform>();
+            foreach (Transform transform in transforms)
+            {
+                if(transform.GetInstanceID() == findInstanceID)
+                {
+                    transform.Ping();
+                    return;
+                }
+            }
+
+            Debug.Log("Did not find Instance ID: " + findInstanceID);
+        }
+
+
+
+    }
+}
