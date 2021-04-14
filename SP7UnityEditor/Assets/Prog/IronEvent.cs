da4f6baf67c793cba46b506dba28df5acf7bee51
AxelSavage <35657878+AxelSavage@users.noreply.github.com>
2021-04-14 15:43:59 +0200

    ladder iteration 1 (not implemented in ironwrought yet!)

---
+++ SP7UnityEditor/Assets/Prog/IronEvent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum IronEventType
+{
+    SoundEffectEvent,
+    GameEvent,
+    VFXEvent
+}
+
+public class IronEvent : MonoBehaviour
+{
+    public IronEventType eventType;
+    public BaseEvent eventObject;
+}
