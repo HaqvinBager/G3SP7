01c1d86ca87c7729643e64ef22692c79e0e80c00
AxelSavage <35657878+AxelSavage@users.noreply.github.com>
2021-04-15 14:17:09 +0200

    no message

---
+++ SP7UnityEditor/Assets/Prog/Editor/Export/ExportBin.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*
+      "Ids": {
+        "byteStart": 0,
+        "sizeof" : 4,
+        "count": 4     
+      },
+      "Transforms": {
+        "byteStart": 20,
+        "sizeof": 28,
+        "count": 4
+      },
+      "Models": {
+        "byteStart": 132,
+        "sizeof": 12,
+        "count" : 1
+      },
+ */
+
+public enum IronType
+{
+    ID,
+    Transform,
+    Model,
+    DirectionalLight,
+    PointLight,
+    Collider,
+    InstancedModel
+}
+
+public struct Chunk
+{
+    public IronType aType;
+    public int byteStart;
+    public int sizeOf;
+    public int count;
+}
+
+public struct BinStructure
+{
+    public List<Chunk> chunks;
+}
+
+public class ExportBin 
+{
+    private string myFileName;
+    private string myDestination;
+    private BinStructure myStructure;
+
+    public ExportBin(string aFileName, string aDestination)
+    {
+        myFileName = aFileName;
+        myDestination = aDestination;
+    }
+
+    public void Add(Chunk aChunk)
+    {
+        myStructure.chunks.Add(aChunk);
+    }
+
+    public void Export()
+    {
+
+    }
+}
